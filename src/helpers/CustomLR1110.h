9dae0a3f0f2f848467ba032d0b5c1e2d28cd9ffd
Florent de Lamotte <florent@frizoncorrea.fr>
2025-02-06 17:07:04 +0100

    test both preamble and header

--- src/helpers/CustomLR1110.h
+++ src/helpers/CustomLR1110.h
@@ -2,6 +2,7 @@
 
 #include <RadioLib.h>
 
+#define LR1110_IRQ_HAS_PREAMBLE                     0b0000000100  //  4     4     valid LoRa header received
 #define LR1110_IRQ_HEADER_VALID                     0b0000010000  //  4     4     valid LoRa header received
 
 class CustomLR1110 : public LR1110 {
@@ -10,7 +11,7 @@ class CustomLR1110 : public LR1110 {
 
     bool isReceiving() {
       uint16_t irq = getIrqStatus();
-      bool hasPreamble = (irq & LR1110_IRQ_HEADER_VALID);
+      bool hasPreamble = ((irq & LR1110_IRQ_HEADER_VALID) && (irq & LR1110_IRQ_HAS_PREAMBLE));
       return hasPreamble;
     }
 };
