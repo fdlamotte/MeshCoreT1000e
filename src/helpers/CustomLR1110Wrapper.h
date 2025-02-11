575bce622fe41726033f33441081069502602548
Florent de Lamotte <florent@frizoncorrea.fr>
2025-02-11 14:27:53 +0100

    sets back preamble length after sending ...

--- src/helpers/CustomLR1110Wrapper.h
+++ src/helpers/CustomLR1110Wrapper.h
@@ -16,6 +16,12 @@ public:
 
     return activity;
   }
+
+  void onSendFinished() override {
+    RadioLibWrapper::onSendFinished();
+    _radio->setPreambleLength(8); // overcomes weird issues with small and big pkts
+  }
+
   float getLastRSSI() const override { return ((CustomLR1110 *)_radio)->getRSSI(); }
   float getLastSNR() const override { return ((CustomLR1110 *)_radio)->getSNR(); }
 };
