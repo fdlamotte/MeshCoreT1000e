8ffcb293f068d30462ff3524e90a6e3013fe41d4
Florent <florent@frizoncorrea.fr>
2025-03-27 21:05:11 +0100

    update build date

--- src/helpers/BaseCompanionRadioMesh.h
+++ src/helpers/BaseCompanionRadioMesh.h
@@ -66,7 +66,7 @@
 #define FIRMWARE_VER_CODE    3
 
 #ifndef FIRMWARE_BUILD_DATE
-  #define FIRMWARE_BUILD_DATE   "25 Mar 2025"
+  #define FIRMWARE_BUILD_DATE   "27 Mar 2025"
 #endif
 
 #ifndef FIRMWARE_VERSION
